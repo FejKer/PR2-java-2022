1cd8707650ebcf258fe96dc881cfbd34727e7c01
Oskar Migowski <o.migowski@protonmail.com>
2022-12-11 15:12:01 +0100

    Zadanie 3 i 4 - get your salary & give yourself a rise

--- src/Human.java
+++ src/Human.java
@@ -1,11 +1,37 @@
+import java.time.LocalDateTime;
+import java.time.format.DateTimeFormatter;
+
 public class Human {
     String firstName;
     String lastName;
     final Integer yearOfBirth;
     Car car;
     Animal pet;
+    private Double salary = 0.0;
+    private LocalDateTime getSalaryDate = null;
+    private Double previousSalary = 0.0;
 
     Human(Integer yearOfBirth){
         this.yearOfBirth = yearOfBirth;
+    }
+
+    public Double getSalary() {
+        System.out.print("Ostatnie pobieranie danych o wypłacie: ");
+        System.out.println(getSalaryDate == null ? "null" : getSalaryDate.format(DateTimeFormatter.ofPattern("yyyy-MM-dd HH:mm")));
+        System.out.println("Poprzednia wypłata: " + previousSalary);
+        getSalaryDate = LocalDateTime.now();
+        previousSalary = salary;
+        return salary;
+    }
+    public void setSalary(Double salary) {
+        if (salary < 0) {
+            System.out.println("Ujemna pensja");
+            return;
+        }
+        System.out.println("Wysłane do księgowości");
+        System.out.println("Pani Hania ma przerwe na kawe to troche potrwa");
+        System.out.println("Smutni panowie ze skarbówki już jadą");
+        this.salary = salary;
+        System.out.println("Ustawiono nową wartość pensji: " + salary);
     }
 }
--- src/Main.java
+++ src/Main.java
@@ -1,6 +1,6 @@
 public class Main {
     public static void main(String[] args) {
-        /*Animal pet = new Animal("fenis");
+        Animal pet = new Animal("fenis");
         pet.feed();
         pet.takeForAWalk();
         pet.takeForAWalk();
@@ -8,10 +8,16 @@ public class Main {
         pet.takeForAWalk();
         pet.takeForAWalk();
         pet.takeForAWalk();
-        pet.feed();*/
+        pet.feed();
 
         Car porsche = new Car("red", 2050.0, "Porsche", "Cayenne");
         Human me = new Human(2002);
+        me.firstName = "Oskar";
+        me.lastName = "MigoFigoFago";
         me.car = porsche;
+        me.pet = pet;
+        System.out.println(me.getSalary());
+        me.setSalary(50000.0);
+        System.out.println(me.getSalary());
     }
 }
